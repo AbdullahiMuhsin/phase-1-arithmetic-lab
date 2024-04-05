61b2d5e84b82bd5a0c12be256958c161c4c35d4e
abdullahi abdi <abdullahimuxsin398@gmail.com>
2024-04-05 08:39:39 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+//Question 1:
+let num3 = 12;
+let num4 = 8
+let mod = num3 % num4;
 
-// Write your code here
+// Question 2:
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; 
+
+//Question 3:
+let random = Math.ceil(Math.random()*1000);
+
+
+
+//Question 4:
+let max = Math.max(4, 8, 1, 20, 9, 16);
